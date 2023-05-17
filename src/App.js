7a8e39269459f29bddcd06fce3de2fc84009b9cc
Maria Rios Navarro <propscode254@gmail.com>
2023-05-17 12:36:42 +0200

    third App

--- src/App.js
+++ src/App.js
@@ -2,5 +2,23 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <div>Replace me with an article</div>;
+  return <ArticleWithAtributes />;
+}
+
+function ArticleWithAtributes() {
+  return (
+    <article className="article">
+      <h2 className="article__title">Atributes in JSX</h2>
+      <label htmlFor="atribut">Atribut</label>
+      <input id="atribut" />
+      <a
+        className="article__link"
+        href={
+          "https://developer.mozilla.org/en-US/docs/Learn/Tools_and_testing/Client-side_JavaScript_frameworks/React_getting_started"
+        }
+      >
+        React MDM
+      </a>
+    </article>
+  );
 }
